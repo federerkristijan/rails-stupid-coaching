be7482fe73cd00ece15ab6194cb6e72da2323fd0
Kristijan Federer <federer.kristijan@gmail.com>
2022-02-14 15:45:07 +0100

    2nd challenge - done

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,15 @@ class QuestionsController < ApplicationController
   def ask
     # something not yet defined
   end
+
+  def answer
+    # something something blablabla
+    if params[:answer] == "I am going to work"
+      @answer = "Great!"
+    elsif params[:answer].include?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
 end
